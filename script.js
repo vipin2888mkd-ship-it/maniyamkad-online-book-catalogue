9a7c0d2e3bb300d10ddea839ccdcdd6aa040ac56
vipin2888mkd-ship-it <vipin2888mkd@gmail.com>
2026-08-05 14:17:08 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-fetch('data/books.json').then(r=>r.json()).then(data=>{
-const box=document.getElementById('books');
-const search=document.getElementById('search');
-function render(list){box.innerHTML=list.map(b=>`<div class='card'><b>${b.code}</b><br>${b.title}<br>${b.author}</div>`).join('');}
-render(data);
-search.oninput=()=>render(data.filter(b=>JSON.stringify(b).toLowerCase().includes(search.value.toLowerCase())));
-});
